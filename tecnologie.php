ccb5c55ca78e701adb06be65c6e60729990d4b17
Blessium <alessandro.melgar04@gmail.com>
2023-05-25 22:26:27 +0200

    Completed tecnology page (db, images, code)

--- tecnologie.php
+++ tecnologie.php
@@ -1,4 +1,10 @@
 <?php
 $title = "Apple - Tecnologie";
+include __DIR__ . "/database.php";
 include __DIR__ . "/pagine/template.html.php";
+$database = Database::get_sigleton();
+$lang = $_SESSION['lang'];
+$page = $database->getPageInfo("TECNOLOGIE", $lang);
+$bruh = $page->generateEverything();
+echo $bruh;
 ?>
